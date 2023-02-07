4f1ec03fb6cc88e4667e90b143bc0e1efd1da663
Pablo Garrido <pablogarrido219@gmail.com>
2023-02-07 12:30:34 -0600

    Cambios a filter y reader

--- csv_reader.py
+++ csv_reader.py
@@ -5,6 +5,7 @@ def csv_reader():
         reader = csv.reader(csvfile, delimiter=',')
         header = next(reader)
         data=[]
+        
         for row in reader:
             iterable =zip(header,row)
             country_dict = {key:value for key, value in iterable}
--- filter.py
+++ filter.py
@@ -3,4 +3,4 @@ import csv_reader
 def filtro(selection,parameter):
     data=csv_reader.csv_reader()
     search=list(filter(lambda item:item[parameter]==selection,data))
-    print(search)
+    print(search[0])
