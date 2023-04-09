70e9ddd735fabaf37fdbee858ed7060c6198ffb3
Pablo Garrido <pablogarrido219@gmail.com>
2023-04-08 19:29:39 -0500

    uso de while en main

--- main.py
+++ main.py
@@ -4,24 +4,34 @@ import buscador
 #according to the request redirect the user to different modules.
 
 def run():
-    try:                                        #Error handling in case of an invalid option.
-        option=int(input("""
-    Write a number search by:
+    print("Welcome to the World Population Finder")
 
-    [1] Population of a country by year
-    [2] Rank,capital,country etc.
+    while True:
+        try:                                        #Error handling in case of an invalid option.
+            option=int(input("""
+        Write a number search by:
+
+        [1] Population of a country by year
+        [2] Rank,capital,country etc.
+
+        [3] Exit ⌫
+            
+        Choose an option:   """))
         
-    Choose an option:   """))
-    
 
-        if option==1:
-            buscador.population()
-        elif option==2:
-            buscador.browser()
-        else:
-            print('That is not an option')
-    except ValueError:
-        print('That is not an option')
+            if option==1:
+                buscador.population()
+            elif option==2:
+                buscador.browser()
+            elif option==3:
+                print('Thank you for using the World Population Finder')
+                break
+            else:
+                print('That is not an option, please select a valid option')
+
+
+        except ValueError:
+            print('That is not an option, please select a valid option')
 
 
 if __name__=='__main__':
